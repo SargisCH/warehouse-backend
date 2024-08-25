1b8932ae9a5fb0a6e865d86bd9f815e29d164fd7
SargisCH <sargis.chilingaryann@gmail.com>
2024-08-25 16:43:58 +0400

    added filter query params for sales

--- src/modules/sale/sale.controller.ts
+++ src/modules/sale/sale.controller.ts
@@ -45,7 +45,10 @@ export class SaleController {
       client?: Array<string>;
       page: number;
       limit: number;
-      created_at: string | Date;
+      manager?: Array<string>;
+      stockProduct?: Array<string>;
+      startDate: string | Date;
+      endDate: string | Date;
     },
   ): Promise<{ saleList: SaleModel[]; totalPages: number }> {
     const user = (request as any).user as User;
@@ -60,22 +63,65 @@ export class SaleController {
         where.client = { managerId: manager.id };
       }
     }
-
     if (Array.isArray(query?.client)) {
       where.clientId = { in: query.client.map((clId) => Number(clId)) };
     } else if (query.client) {
       where.clientId = Number(query.client);
     }
+    if (Array.isArray(query?.stockProduct)) {
+      where.saleItems = {
+        some: {
+          stockProduct: {
+            id: { in: query.stockProduct.map((clId) => Number(clId)) },
+          },
+        },
+      };
+    } else if (query?.stockProduct) {
+      where.saleItems = {
+        some: {
+          stockProduct: {
+            id: Number(query.stockProduct),
+          },
+        },
+      };
+    }
+    if (Array.isArray(query?.manager)) {
+      where.client = {
+        managerId: { in: query.manager.map((mnId) => Number(mnId)) },
+      };
+    } else if (query.manager) {
+      where.client = { managerId: Number(query.manager) };
+    }
     const page = query?.page || 1;
     const limit = query?.limit || 10;
     const skip = !page ? 0 : (page - 1) * limit;
     const take = parseInt(String(limit));
-    const createdAt = dayjs(query.created_at, 'YYYY-MM-DD', true);
+    const startDate = dayjs(query.startDate, 'YYYY-MM-DD', true);
+    let endDate;
+    if (query.endDate) {
+      endDate = dayjs(query.endDate, 'YYYY-MM-DD', true);
+    }
 
-    const startOfDay = createdAt.startOf('day');
-    const endOfDay = createdAt.endOf('day');
+    const startOfDay = startDate.startOf('day');
 
-    if (query.created_at && createdAt.isValid()) {
+    let endOfDay;
+    if (query.endDate) {
+      endOfDay = endDate.endOf('day');
+    }
+
+    if (query.startDate && startDate.isValid() && !query.endDate) {
+      const end = startDate.endOf('day');
+      where.created_at = {
+        gte: startOfDay.toISOString(),
+        lte: end.toISOString(),
+      };
+    } else if (
+      query.startDate &&
+      query.endDate &&
+      startDate.isValid() &&
+      endDate &&
+      endDate.isValid()
+    ) {
       where.created_at = {
         gte: startOfDay.toISOString(),
         lte: endOfDay.toISOString(),
